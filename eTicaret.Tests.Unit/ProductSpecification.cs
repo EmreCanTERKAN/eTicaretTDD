65e7361355d99790ed35bfd3ba64e27066a62b52
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-06-18 14:25:53 +0300

    İlk test başarıyla tamamlandı.

--- eTicaret.Tests.Unit/ProductSpecification.cs
+++ eTicaret.Tests.Unit/ProductSpecification.cs
@@ -1,14 +1,30 @@
-﻿namespace eTicaret.Tests.Unit;
+﻿using FluentAssertions;
+
+namespace eTicaret.Tests.Unit;
 
 public class ProductSpecification
 {
     [Fact]
-    public void Create_Should_Be_Throw_Exception_If_Name_Less_Then_3_Chars()
+    public async Task Create_Should_Be_Throw_Exception_If_Name_Less_Then_3_Chars()
     {
         // Arrange
-
+        CreateProductCommand request = new("La");
+        CreateProductCommandHandler command = new();
         // Act
-
+        var action = async () => await command.Handle(request, default);
         // Assert
+        await action.Should().ThrowAsync<Exception>();
+    }
+}
+
+
+public sealed record CreateProductCommand(string name);
+
+public sealed class  CreateProductCommandHandler
+{
+    public async Task Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        throw new Exception("Name must be greater than 3 characters");
+        await Task.CompletedTask;
     }
 }
